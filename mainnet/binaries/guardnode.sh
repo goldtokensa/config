c7f586ff618f3322690cc40416efc6489536211b
tomossomot <tomoswootton@protonmail.com>
2020-01-31 10:51:51 +0000

    update coordinator url

--- mainnet/binaries/guardnode.sh
+++ mainnet/binaries/guardnode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec $HOME/guardnode/run_guardnode --rpcuser ocean --rpcpass oceanpass --rpchost 127.0.0.1:8332 --servicerpcuser ocean --servicerpcpass oceanpass --servicerpchost 127.0.0.1:8332 --nodelogfile ../ocean/gold_main/debug.log --challengehost https://coordinator.gtsa.io:443 --bidlimit 50
+exec $HOME/guardnode/run_guardnode --rpcuser ocean --rpcpass oceanpass --rpchost 127.0.0.1:8332 --servicerpcuser ocean --servicerpcpass oceanpass --servicerpchost 127.0.0.1:8332 --nodelogfile ../ocean/gold_main/debug.log --challengehost https://coordinator.mainnet.gtsa.io:10007 --bidlimit 50
